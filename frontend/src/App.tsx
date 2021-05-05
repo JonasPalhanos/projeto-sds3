b9368da504b96a5c68b8350d48e784c80f33fb2c
Jonas Palhanos <jonaspalhanos@hotmail.com>
2021-05-05 02:44:26 -0400

    Static charts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -14,11 +14,11 @@ function App() {
         <div className="row px-3">
           <div className="col-sm-6">
               <h5 className="text-center text-secondary">Taxa de sucesso (%)</h5>
-              <BarChart />
+              <BarChart/>
           </div>
           <div className="col-sm-6">
               <h5 className="text-center text-secondary">Todas vendas</h5>
-              <DonutChart />
+              <DonutChart/>
           </div>
         </div>
 
